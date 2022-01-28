248643d87a4ddcb69203d21d7e78e432360fc5aa
Lucas Jorgensen <lucassjorgensen@gmail.com>
2022-01-28 14:13:36 -0500

    Progress on ex02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,40 @@
+"""Ex02 One shot Wordle."""
+__author__ = "730484794"
+
+from platform import python_branch
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+secret: str = "python"
+guess: str = input("What is your 6-letter guess? ")
+i: int = 0
+emoji: str = ""
+anywhere: bool = False
+j: int = 0
+
+
+while len(guess) != len(secret):
+    guess = input("That was not 6 letters! Try again: ")
+
+while i < len(secret):
+    if guess[i] == secret[i]:
+        emoji += GREEN_BOX
+    else:
+        while anywhere == False and j < len(secret):
+            if guess[i] == secret[j]:
+                anywhere = True
+            j += 1
+        if anywhere:
+            emoji += YELLOW_BOX
+        else:
+            emoji += WHITE_BOX
+    i += 1
+    j = 0
+    anywhere = False
+print(emoji)
+
+if guess == secret:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Try again soon!")
--- exercises/test_space.py
+++ exercises/test_space.py
@@ -1,5 +1,6 @@
 from random import choice
 
+print("Hello\nworld\n!!!")
 print(10 // 2)
 print(choice("asdf"))
 print("U" + "N" + "C")
@@ -10,3 +11,10 @@ print(2%3)
 x: int = 1
 x = x+1
 print(x)
+if x == 1:
+    print("true")
+else:
+    print("false")
+age: int = 21
+msg: str = f"You are {age}!"
+print(msg)
